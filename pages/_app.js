b1cb89cf8652f6ce3422a2c4d5b90b08f41d693f
al3des <alejandrodesalvo@gmail.com>
2021-09-02 16:15:12 +0200

    added material-ui and cards to search results

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+import { ThemeProvider, makeStyles, CssBaseline, createTheme } from '@material-ui/core'
+
+
+const theme = createTheme({
+ 
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+    <meta
+  name="viewport"
+  content="minimum-scale=1, initial-scale=1, width=device-width"
+/>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" /></Head>
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider></>
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import { useEffect, useRef, useState } from "react"
-
+import clsx from 'clsx';
+import { Grid, Card, CardActions, CardContent, CardHeader, CardMedia, Collapse, Container, IconButton, makeStyles, Typography } from "@material-ui/core"
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Image from "next/image"
 
 import axios from "axios"
@@ -13,13 +17,53 @@ function getQuestions(productId, questions) {
     .map((x) => x.questions)
 }
 
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  pill: {
+    backgroundColor: '#f8beed',
+    color: '#3f2f47',
+    border: 'none',
+    padding: '10px 20px',
+    textAlign: 'center',
+    textDecoration: 'none',
+    display: 'inline-block',
+    margin: '4px 2px',
+    borderRadius: '16px',
+  },
+  avatar: {
+    // backgroundColor: red[500],
+  },
+}));
+
 export default function Home() {
   const [search, setSearch] = useState("")
   const [results, setResults] = useState([])
   const [questions, setQuestions] = useState([])
   const [descriptions, setDescriptions] = useState([])
   const ref = useRef(null)
+  const classes = useStyles()
+  const [expanded, setExpanded] = useState(false);
 
+  const handleExpandClick = (panel) => (event, isExpanded) => {
+    setExpanded({ [panel]: !expanded[panel] });
+  };
   const url = "https://api.mercadolibre.com"
 
   useEffect(() => {
@@ -61,47 +105,77 @@ export default function Home() {
       )
   }, [results])
 
-  console.log(questions)
 
   return (
-    <div className={`${styles.container} `}>
+    <Container
+    // className={`${styles.container} `}
+    >
       <button onClick={() => setSearch("rx480 8gb")}>RX 480 8GB</button>
       <button onClick={() => setSearch("rx580 8gb")}>RX 580 8GB</button>
       <button onClick={() => setSearch("rx570 8gb")}>RX 570 8GB</button>
       <button onClick={() => setSearch("rx5700 xt 8gb")}>RX 5700 xt 8GB</button>
 
-      <div>
+      <Grid container>
         {results.map((p) => (
-          <div className={styles.card}>
-            <a href={p.permalink} target="_blank">
+          <Grid item key={p.id}>
+            <Card
+              className={classes.root}>
+              <a href={p.permalink} target='_blank'>
+                <CardHeader title={p.title} />
+                </a>
+
+              {/* <a href={p.permalink} target="_blank">
               <h1>{p.title}</h1>
-            </a>
-            <Image src={p.thumbnail} height={200} width={200} />
-            <p>$ {p.price.toLocaleString("en-US")}</p>
-              <adddress>{p.address.state_name}, {p.address.city_name}</adddress>
-              {p.shipping.free_shipping && <div className={styles.pill}>envio gratis</div>}
-              {p.accepts_mercadopago && <div className={styles.pill}>acepta mercado pago</div>}
-            <p>
-              {descriptions &&
-                descriptions
-                  .filter((d) => d.item_id == p.id)
-                  .map((e) => e.description)}
-            </p>
-            <div className={styles.accordion}>
-              {questions &&
-                getQuestions(p.id, questions).map((q) =>
-                  q.map((q) => (
-                    <>
-                      <p className="bold">{q.text}</p>
-                      <p>{q.answer && q.answer.text}</p>
-                    </>
-                  ))
-                )}
-            </div>
-          </div>
+            </a> */}
+              <CardMedia className={classes.media} image={p.thumbnail} />
+              <CardContent>
+
+                <Typography variant='h4'>$ {p.price.toLocaleString("en-US")}</Typography>
+                <Typography paragraph variant="caption"><LocationOnIcon />{p.address.state_name}, {p.address.city_name}</Typography>
+                {p.shipping.free_shipping && <Typography paragraph variant='caption' className={classes.pill}>envio gratis</Typography>}
+                {p.accepts_mercadopago && <Typography paragraph variant='caption' className={classes.pill}>acepta mercado pago</Typography>}
+
+                {descriptions &&
+                  descriptions
+                    .filter((d) => d.item_id == p.id)
+                    .map((e) => <Typography key={p.id} paragraph noWrap={!expanded[p.id]}>{e.description}</Typography>)}
+
+
+              </CardContent>
+              <CardActions>
+                <IconButton aria-label="add to favorites">
+                  <FavoriteIcon />
+                </IconButton>
+                <Typography>Preguntas: </Typography>
+                <IconButton
+                  className={clsx(classes.expand, {
+                    [classes.expandOpen]: expanded[p.id],
+                  })}
+
+                  onClick={handleExpandClick(p.id)}
+                  aria-expanded={expanded[p.id]}
+                  aria-label="show more"
+                >
+                  <ExpandMoreIcon />
+                </IconButton>
+              </CardActions>
+              <Collapse in={expanded[p.id]} timeout="auto" unmountOnExit>
+
+                {questions &&
+                  getQuestions(p.id, questions).map((q) =>
+                    q.map((q) => (
+                        <CardContent key={q.id} className={styles.accordion}>
+                          <Typography paragraph variant='subtitle2'>{q.text}</Typography>
+                          <Typography paragraph >{q.answer && q.answer.text}</Typography>
+                        </CardContent>
+                    ))
+                  )}
+              </Collapse>
+            </Card>
+          </Grid>
         ))}
-      </div>
-    </div>
+      </Grid>
+    </Container>
   )
 }
 
